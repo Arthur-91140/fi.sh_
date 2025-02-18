fd6d989f1a50ebcfb47ee28598a938c90f0f4072
igibi1791 <tuture77150@gmail.com>
2025-02-18 21:06:34 +0100

    ajout command.h

---
+++ include/command.h
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <wininet.h>
+#include <shlobj.h>
+#include <cstdlib>
+#include <tchar.h>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "../include/core.h"
+
+using namespace std;
+
+#pragma comment(lib, "wininet.lib")
+
+// =============================================================
+// Prototypes relatifs au commandes éxécutables par le main
+// =============================================================
