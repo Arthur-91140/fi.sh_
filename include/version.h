bddaa4ee1c60a52ce24ad91d507620e61f8afc3d
Arthur Pruvost Rivière <96424611+igibi1791@users.noreply.github.com>
2025-03-04 11:56:35 +0100

    add include definition

---
+++ include/version.h
@@ -0,0 +1,16 @@
+#ifndef version_H_INCLUDED
+#define version_H_INCLUDED
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <windows.h>
+
+#pragma comment(lib, "wininet.lib")
+
+using namespace std;
+
+
+
+#endif
